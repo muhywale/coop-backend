e6d68c2fdbaa99bfb703c44741b2a14a45bb7566
muhywale <lilmuhy007@gmail.com>
2026-09-01 19:26:59 +0100

    cookie setting

--- controllers/authController.js
+++ controllers/authController.js
@@ -94,7 +94,6 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      // eslint-disable-next-line no-undef
       secure: true, // HTTPS only in production
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
