620c54ee699d13793d22f9593b7159276f969357
muhywale <lilmuhy007@gmail.com>
2026-09-01 06:53:23 +0100

    routes members

--- routes/members.js
+++ routes/members.js
@@ -38,7 +38,6 @@ const handleValidation = (req, res, next) => {
   }
   next();
 };
-router.get("/", verifyToken, getMembers);
 
 router.get("/me/detail", verifyToken, getMyDetail);
 router.get("/me/ledger", verifyToken, getMyLedger);
@@ -57,6 +56,7 @@ router.get(
 );
 router.get("/:id", verifyToken, getMemberById);
 router.put("/:id", verifyToken, updateMember);
+router.get("/", verifyToken, getMembers);
 
 router.post(
   "/",
